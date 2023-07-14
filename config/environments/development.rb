5d27cb5be01aa1063e5c4f5a6bdcf95a758fc047
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-07-14 06:51:52 +0000

    Add Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "b166382a77384f98b2dc97a25d6b51fc.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
