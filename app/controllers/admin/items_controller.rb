429373c64b8365552e93243db3edd72e4604d5b1
EC2 Default User <ec2-user@ip-172-31-7-228.ap-northeast-1.compute.internal>
2023-07-15 07:20:54 +0000

    add new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,20 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
-    @items = Item.all 
+    @items = Itempage.(params[:page])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @Item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(item)
+  end
+
+  def item_params
+    params.require(:item).permit(:name,:introduction,:price,:ganre_id,:is_available)
   end
 end
