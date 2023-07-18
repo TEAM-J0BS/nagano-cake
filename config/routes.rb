78a08546a9583ab17acba6dda54eca83636b6c58
EC2 Default User <ec2-user@ip-172-31-7-228.ap-northeast-1.compute.internal>
2023-07-18 07:28:40 +0000

    add about

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
 scope module: :public do
   root to: "homes#top"
+  get "/home/about"=>"homes#about", as: "about"
   get "customers/mypage" => "customers#show"
   get "customers/information/edit" => "customers#edit"
   patch "customers/information" => "customers#update"
