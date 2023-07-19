861944267e01a240e27bb007892a53cadf42d7f9
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-07-19 13:03:31 +0000

    update admin/index

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ApplicationRecord
 
   enum payment_method: { credit_card: 0, transfer: 1 }
 
-  enum status:{
+  enum status: {
     waiting_for_payment:0, payment_confirmation:1, in_production:2, shipping_preparation:3, sent:4
   }
 
