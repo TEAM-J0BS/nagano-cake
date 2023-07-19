d2c4f2461eb07d80a8561cc959612921d5d3f342
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-07-19 07:53:06 +0000

    add ja.yml

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,8 @@
 class Admin::OrdersController < ApplicationController
   def index
+    @orders = Order.all.order("created_at desc")
   end
-  
+
   def show
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,12 +5,12 @@ class Order < ApplicationRecord
 
   enum payment_method: { credit_card: 0, transfer: 1 }
 
- enum status:{
-  "入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済み":4
- }
- 
+  enum status:{
+    waiting_for_payment:0, payment_confirmation:1, in_production:2, shipping_preparation:3, sent:4
+  }
+
   def self.ransackable_attributes(auth_object = nil)
     ["name","introduction"]
   end
- 
+
 end
