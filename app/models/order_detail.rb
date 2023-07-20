d9b5df399be00ff6c41cf14237a6d411078a1d1b
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-07-20 02:24:52 +0000

    jaのエラー解消

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -5,4 +5,9 @@ class OrderDetail < ApplicationRecord
   enum making_status: {
     impossible: 0, wait: 1, making: 2, finish: 3
   }
+  
+    def total_price
+    item.with_tax_price.to_i * count.to_i
+  end
+  
 end
