4f18d0cad3de27f669aa4c47debdd7b03b358c1c
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-07-20 02:25:31 +0000

    fix model

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,6 @@ class Order < ApplicationRecord
   has_many :order_details
 
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum status:         { wait: 0, check: 1, making: 2, standby: 3, sent: 4 }
-
-　 def self.ransackable_attributes(auth_object = nil)
-    ["name","introduction"]
-  end
+  enum status: { wait: 0, check: 1, making: 2, standby: 3, sent: 4 }
 
 end
